980115a6d84c1cece9bdd17fe327a42423853f03
Anthony Incer <eyecanthony@gmail.com>
2015-02-12 16:46:47 -0500

    Category to product

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150211204154) do
+ActiveRecord::Schema.define(version: 20150211222123) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20150211204154) do
     t.integer  "price_in_cents"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+    t.string   "category"
   end
 
   create_table "reviews", force: :cascade do |t|
