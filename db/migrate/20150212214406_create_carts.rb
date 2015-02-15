d792428b42b842af15795ce9c86240f60091cf82
Jarf <crazymoose7@gmail.com>
2015-02-15 12:13:33 -0500

    Fixing migration errors

--- db/migrate/20150212214406_create_carts.rb
+++ db/migrate/20150212214406_create_carts.rb
@@ -7,5 +7,15 @@ class CreateCarts < ActiveRecord::Migration
       t.timestamps null: false
     end
     add_foreign_key :carts, :users
+
+    create_table :cart_products do |t|
+      t.references :product, index: true
+      t.references :cart, index: true
+
+      t.timestamps null: false
+    end
+ 
+    add_foreign_key :cart_products, :products
+    add_foreign_key :cart_products, :carts
   end
 end
